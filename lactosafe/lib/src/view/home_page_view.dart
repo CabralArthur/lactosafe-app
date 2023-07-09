13fcb570a2d68fb6bd1b92336b323b67f67bd400
Graziela Felix <grazielafelix.dev@gmail.com>
2023-07-09 01:21:06 -0300

    feat: barra de navegação inferior

--- lactosafe/lib/src/view/home_page_view.dart
+++ lactosafe/lib/src/view/home_page_view.dart
@@ -11,6 +11,27 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        actions: [
+          Row(
+            children: <Widget>[
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.person),
+                color: Colors.orange,
+              )
+            ],
+          )
+        ],
+      ),
+      body: Container(width: double.maxFinite, child: ListView()),
+      bottomNavigationBar: BottomNavigationBar(items: const [
+        BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+        BottomNavigationBarItem(icon: Icon(Icons.camera), label: 'Camera'),
+        BottomNavigationBarItem(icon: Icon(Icons.map), label: 'Mapa')
+      ]),
+    );
   }
 }
