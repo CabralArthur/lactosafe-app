55827e8a0fd12a0d3c9d50bd9ab547378e0bd468
Graziela Felix <grazielafelix.dev@gmail.com>
2023-07-12 19:14:21 -0300

    feat: camera page view

---
+++ lactosafe/lib/src/view/camera_view.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class CameraPage extends StatefulWidget {
+  const CameraPage({super.key});
+
+  @override
+  State<CameraPage> createState() => _CameraPageState();
+}
+
+class _CameraPageState extends State<CameraPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text("Camera"),
+      ),
+    );
+  }
+}
