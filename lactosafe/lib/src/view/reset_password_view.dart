8cd51873e2878fc85be438f45fb33f9bc9e211e3
carlosgabriel311 <carlosgabriel123456@gmail.com>
2023-07-26 22:12:17 -0300

    add-back_route_and_validator

--- lactosafe/lib/src/view/reset_password_view.dart
+++ lactosafe/lib/src/view/reset_password_view.dart
@@ -16,37 +16,42 @@ class _ResetPasswordState extends State<ResetPassword> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Container(
-            width: MediaQuery.of(context).size.width * 0.8,
-            height: MediaQuery.of(context).size.height * 0.8,
-            child: Form(
-              key: _formkey,
-              child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    const Align(
-                      alignment: Alignment.centerLeft,
-                      child: Padding(
-                        padding: EdgeInsets.all(8),
-                        child: Text('Informe o Email da conta'),
-                      ),
+        child: SizedBox(
+          width: MediaQuery.of(context).size.width * 0.8,
+          height: MediaQuery.of(context).size.height * 0.8,
+          child: Form(
+            key: _formkey,
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  const Align(
+                    alignment: Alignment.centerLeft,
+                    child: Padding(
+                      padding: EdgeInsets.all(8),
+                      child: Text('Informe o Email da conta'),
                     ),
-                    Padding(
-                      padding: const EdgeInsets.all(8),
-                      child: CustomTextField(
-                        icon: Icons.email,
-                        label: 'Email',
-                        validator: (value) {
-                          if (value == null || value.isEmpty) {
-                            return 'Informe algum email';
-                          }
-                          return null;
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8),
+                    child: CustomTextField(
+                      icon: Icons.email,
+                      label: 'Email',
+                      validator: (value) => validateForm(value),
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      TextButton(
+                        onPressed: () {
+                          Navigator.pop(context);
                         },
+                        child: const Text(
+                          'Voltar',
+                          style: TextStyle(color: Colors.deepOrangeAccent),
+                        ),
                       ),
-                    ),
-                    Align(
-                      alignment: Alignment.bottomRight,
-                      child: SizedBox(
+                      SizedBox(
                         height: 50,
                         child: ElevatedButton(
                           onPressed: () => submitForm(_formkey, context),
@@ -59,10 +64,12 @@ class _ResetPasswordState extends State<ResetPassword> {
                             ],
                           ),
                         ),
-                      ),
-                    )
-                  ]),
-            )),
+                      ), 
+                    ],
+                  )
+                ]),
+          ),
+        ),
       ),
     );
   }
