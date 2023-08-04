913b094b3880f435da002a614457d7120a854446
GrazielaFelix <grazielafelix.dev@gmail.com>
2023-08-04 17:32:51 -0300

    feat: adicionando nova cor 'shadow' ao app

--- lactosafe/lib/src/shared/app_colors.dart
+++ lactosafe/lib/src/shared/app_colors.dart
@@ -3,4 +3,5 @@ import 'package:flutter/material.dart';
 class AppColors {
   static Color orange = const Color(0xC1ED5500);
   static Color grey = const Color(0x991E1E1E);
+  static Color shadow = const Color.fromARGB(15, 0, 0, 0);
 }
