2547e584fdefd05bc9d29cc5ceb2f11127eb07db
GrazielaFelix <grazielafelix.dev@gmail.com>
2023-08-04 17:35:22 -0300

    feat: adicionando barra de pesquisa e testando layout inicial dos cards do filtro e historico

--- lactosafe/lib/src/view/home_page_view.dart
+++ lactosafe/lib/src/view/home_page_view.dart
@@ -1,3 +1,8 @@
+import 'package:LactoSafe/src/components/custom_text_field.dart';
+import 'package:LactoSafe/src/components/photo_widget.dart';
+import 'package:LactoSafe/src/shared/app_colors.dart';
+import 'package:LactoSafe/src/shared/app_images.dart';
+import 'package:LactoSafe/src/shared/app_settings.dart';
 import 'package:flutter/material.dart';
 
 class HomePageBody extends StatefulWidget {
@@ -11,181 +16,152 @@ class _HomePageBodyState extends State<HomePageBody> {
   @override
   Widget build(BuildContext Context) {
     return Scaffold(
-      body: ListView(
+      body: Padding(
         padding: const EdgeInsets.all(26.0),
-        children: [
-          //Procure Produtos
-          const SizedBox(
-            width: 264,
-            child: Text(
-              'Procure produtos ',
-              style: TextStyle(
-                color: Color(0xC11E1E1E),
-                fontSize: 24,
-                fontFamily: 'Roboto',
-                fontWeight: FontWeight.w900,
-                letterSpacing: 2.40,
+        child: Column(
+          children: [
+            //Procure Produtos
+            SizedBox(
+              width: AppSettings.screenWidth,
+              child: const Text(
+                'Procure produtos ',
+                style: TextStyle(
+                  color: Color(0xC11E1E1E),
+                  fontSize: 24,
+                  fontFamily: 'Roboto',
+                  fontWeight: FontWeight.w900,
+                  letterSpacing: 2.40,
+                ),
               ),
             ),
-          ),
-
-          //Barra de Pesquisa
-          SizedBox(
-            width: 337,
-            height: 56,
-            child: Stack(
-              children: [
-                Positioned(
-                    left: 0,
-                    right: 0,
-                    child: Container(
-                      width: 337,
-                      height: 56,
-                      decoration: ShapeDecoration(
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(25)),
-                          color: const Color(0x66D9D9D9)),
-                    )),
-                Positioned(
-                  left: 18,
-                  top: 17,
-                  child: SizedBox(
-                    width: 25.32,
-                    height: 22,
-                    child: Stack(
-                      children: [
-                        Positioned(
-                          left: 0,
-                          top: 0,
-                          child: Container(
-                            width: 22,
-                            height: 22,
-                            decoration: const ShapeDecoration(
-                              shape: OvalBorder(
-                                side: BorderSide(
-                                    width: 1, color: Color(0xC11E1E1E)),
-                              ),
-                            ),
-                          ),
-                        ),
-                        Positioned(
-                          left: 19.47,
-                          top: 17.51,
-                          child: Transform(
-                            transform: Matrix4.identity()
-                              ..translate(0.0, 0.0)
-                              ..rotateZ(0.63),
-                            child: Container(
-                              width: 7.24,
-                              decoration: const ShapeDecoration(
-                                shape: RoundedRectangleBorder(
-                                  side: BorderSide(
-                                    width: 1,
-                                    strokeAlign: BorderSide.strokeAlignCenter,
-                                    color: Color(0xC11E1E1E),
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ),
-                        ),
+      
+            SizedBox(height: AppSettings.screenHeight/30,),
+      
+            //Barra de Pesquisa
+      
+            const CustomTextField(icon: Icons.search_rounded, label: "Pesquisar", iconSize: 28.0,),
+      
+            SizedBox(height: AppSettings.screenHeight/20,),
+      
+            // Historico de pesquisa
+           SizedBox(
+              width: AppSettings.screenWidth,
+              height: AppSettings.screenHeight/22,
+              child: const Text(
+                'Histórico de pesquisa',
+                style: TextStyle(
+                  color: Color(0xC11E1E1E),
+                  fontSize: 21,
+                  fontFamily: 'Roboto',
+                  fontWeight: FontWeight.w900,
+                ),
+              ),
+            ),
+      
+            // filtro de pesquisa
+      
+            SizedBox(
+              height: AppSettings.screenHeight/12,
+                child:
+                  SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: Row (children: <Widget>[ 
+                            buildFilter('MUITO ALTO'),
+                      buildFilter('ALTO'),
+                      buildFilter('MÉDIO'),
+                      buildFilter('BAIXO'),
+                      buildFilter('INEXISTENTE'),
                       ],
-                    ),
-                  ),
-                ),
-                const Positioned(
-                  left: 64,
-                  top: 17,
-                  child: SizedBox(
-                    width: 180,
-                    height: 18,
-                    child: Text(
-                      'Pesquisar',
-                      style: TextStyle(
-                        color: Color(0x991E1E1E),
-                        fontSize: 18,
-                        fontFamily: 'Roboto',
-                        fontWeight: FontWeight.w400,
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-
-          // Historico de pesquisa
-          const SizedBox(
-            width: 212,
-            height: 34,
-            child: Text(
-              'Histórico de pesquisa',
-              style: TextStyle(
-                color: Color(0xC11E1E1E),
-                fontSize: 21,
-                fontFamily: 'Roboto',
-                fontWeight: FontWeight.w900,
+            
+                    )),  
+              
               ),
-            ),
-          ),
+            Expanded(child: ListView(scrollDirection: Axis.horizontal,
+                    children: <Widget>[
+                      buildFoodCard()
 
-          // filtro de pesquisa
-          // SizedBox(
-          //   //height: 26,
-          //   child: ListView(
-          //     scrollDirection: Axis.horizontal,
-          //     children: <Widget>[
-          //       buildFilter('MUITO ALTO'),
-          //       buildFilter('ALTO'),
-          //       buildFilter('ALTO'),
-          //       buildFilter('ALTO'),
-          //       buildFilter('ALTO')
-          //     ],
-          //   ),
-          // )
-        ],
+                    ],),)
+          ],
+        ),
       ),
     );
   }
 
-  Widget buildFilter(String filterName) => SizedBox(
-        width: 65,
-        height: 26,
-        child: Stack(
-          children: [
-            Positioned(
-              left: 0,
-              top: 0,
-              child: Container(
-                width: 65,
-                height: 26,
-                decoration: ShapeDecoration(
-                  color: Colors.white,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(30),
-                  ),
-                ),
-              ),
-            ),
-            const Positioned(
-              left: 0,
-              top: 0,
-              child: SizedBox(
-                width: 65,
-                height: 26,
-                child: Text(
-                  'teste',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    color: Color(0xFF1E1E1E),
-                    fontSize: 10,
-                    fontFamily: 'Roboto',
-                    fontWeight: FontWeight.w400,
-                  ),
-                ),
-              ),
-            ),
-          ],
-        ),
-      );
+  Widget buildFilter(String filterName) => Container(
+        alignment: Alignment.center,
+        margin: const EdgeInsets.all(12),
+        width: AppSettings.screenWidth/4,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(30), 
+          color: Colors.white,
+          boxShadow: [BoxShadow(color: AppColors.shadow, blurRadius: 10, offset: Offset.fromDirection(-2, -5), spreadRadius: 0.1, blurStyle: BlurStyle.normal )]
+          ),
+        child: Center(child: Text(filterName)),);
 }
+
+Widget buildFoodCard() => Stack(
+  alignment: Alignment.center,
+
+  children: [
+        Container(
+      alignment: Alignment.topCenter,
+    
+            margin: const EdgeInsets.all(12),
+    
+            width: AppSettings.screenWidth/2+40,
+    
+            decoration: BoxDecoration(
+    
+              borderRadius: BorderRadius.circular(30), 
+    
+              color: Colors.white,
+    
+              boxShadow: [BoxShadow(color: AppColors.shadow, blurRadius: 10, offset: Offset.fromDirection(-2, -5), spreadRadius: 0.1, blurStyle: BlurStyle.normal )]
+    
+              ), child: Stack(
+    
+                alignment: Alignment.topCenter, 
+    
+                children: [Container(
+    
+                  height: AppSettings.screenHeight/4,
+    
+                  width: AppSettings.screenWidth/2,
+    
+                  decoration: BoxDecoration(
+    
+              borderRadius: BorderRadius.circular(30), 
+    
+              color: AppColors.grey,
+    
+              boxShadow: [BoxShadow(color: AppColors.shadow, blurRadius: 10, offset: Offset.fromDirection(-2, -5), spreadRadius: 0.1, blurStyle: BlurStyle.normal )]
+    
+              )
+    
+                  ), 
+    
+                  PhotoWidget(picture: AppImages.cremeDeLeite, height: AppSettings.screenHeight/6,),
+    
+                  Container(
+    
+                  height: AppSettings.screenHeight/6,
+    
+                  decoration: BoxDecoration(
+    
+                    color: Colors.white, 
+    
+                    shape: BoxShape.circle,
+    
+                    image: DecorationImage(image: AssetImage(AppImages.cremeDeLeite,), fit: BoxFit.contain)),
+    
+                  
+    
+                  )
+    
+                  
+    
+                  ]),
+    
+    ),
+  ],
+);
