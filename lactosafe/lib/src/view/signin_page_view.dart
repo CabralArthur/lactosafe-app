b99a58ac14d4c8fa577972d17072ac9f5be7a22d
João Vitor da Silva Pires <jvitorpires1999@gmail.com>
2023-08-07 07:22:59 -0300

    Overflow Fix

--- lactosafe/lib/src/view/signin_page_view.dart
+++ lactosafe/lib/src/view/signin_page_view.dart
@@ -12,132 +12,122 @@ class SignIn extends StatefulWidget {
 }
 
 class _SignInState extends State<SignIn> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-
-      body: Column(
-        children: [
-          Expanded(
-            flex: 2,
-            child: Container(
-              color: Colors.white,
-              child: Image.asset(AppImages.logo)          
+        backgroundColor: Colors.white,
+        body: Column(
+          children: [
+            Expanded(
+              flex: 2,
+              child: Container(
+                  color: Colors.white, child: Image.asset(AppImages.logo)),
             ),
-          ),
-          Expanded(
-            flex: 3,
-            child: Container(
-              padding: const EdgeInsets.symmetric(
-                horizontal: 32,
-                vertical: 50,
-
-              ),
-              
-              decoration: const BoxDecoration(
-
-                color: Colors.white,
-
-                borderRadius: BorderRadius.vertical(
-                  top: Radius.circular(45)
-                )
-              ),
-              child:  Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  
-                  //Email
-                  const CustomTextField(
-                    icon: Icons.email,
-                    label: 'Email',
-                  ),
-                  //Senha
-                  const CustomTextField(
-                    icon: Icons.lock,
-                    label: 'Senha',
-                    isObscure: true,
-                  ),
-                  //Esqueceu a senha
-                  Align(
-                    alignment: Alignment.centerRight,
-                    child: TextButton(onPressed: () {
-                      Navigator.pushNamed(context, '/resetPassword');
-                    }, 
-                    child: Text('Esqueceu a senha?', 
-                    style: TextStyle(
-                      color: AppColors.orange), 
-                      ),
-                    ),
-                  ),
-
-                  //Entrar
-                  SizedBox(
-                    height: 50,
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(18))
-                      ),
-                      onPressed: () {
-                        Navigator.popAndPushNamed(context, '/home');
-                      }, 
-                      child: const Text(
-                        'Entrar', style: TextStyle(
-                          fontSize: 18
-                        ),),
-                      
-                      ),                      
-                  ),
-                  
-                  const Padding (
-                    padding: EdgeInsets.symmetric(vertical: 16),
-                    child: Text(
-                      'ou',
-                      style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold, 
-                      ),
-                    ),
-                  ),
-                  // GoogleButton
-                  InkWell(
-                    onTap: (){
-                      //Vai pro login com google
-                    },
-                    child: Container(
-                      child: Image.asset(
-                        AppImages.googleIcon,
-                        width: 60,
-                        height: 50,
-                        fit: BoxFit.contain,
-                      ),
-                    ),
-                  ),
-                  ///CadastroButton
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
+            Expanded(
+              flex: 3,
+              child: Container(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 32,
+                  vertical: 50,
+                ),
+                decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius:
+                        BorderRadius.vertical(top: Radius.circular(45))),
+                child: SingleChildScrollView(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
-                      const Text(
-                        'Não possui uma conta?', 
-                        style: TextStyle(fontSize: 15),
+                      //Email
+                      const CustomTextField(
+                        icon: Icons.email,
+                        label: 'Email',
                       ),
-                      TextButton(onPressed: (){}, 
-                      child: Text(
-                        'Cadastre-se aqui!', 
-                        style:  TextStyle(fontSize: 15, color: AppColors.orange),
+                      //Senha
+                      const CustomTextField(
+                        icon: Icons.lock,
+                        label: 'Senha',
+                        isObscure: true,
+                      ),
+                      //Esqueceu a senha
+                      Align(
+                        alignment: Alignment.centerRight,
+                        child: TextButton(
+                          onPressed: () {
+                            Navigator.pushNamed(context, '/resetPassword');
+                          },
+                          child: Text(
+                            'Esqueceu a senha?',
+                            style: TextStyle(color: AppColors.orange),
+                          ),
+                        ),
+                      ),
+
+                      //Entrar
+                      SizedBox(
+                        height: 50,
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(18))),
+                          onPressed: () {
+                            Navigator.popAndPushNamed(context, '/home');
+                          },
+                          child: const Text(
+                            'Entrar',
+                            style: TextStyle(fontSize: 18),
+                          ),
+                        ),
+                      ),
+
+                      const Padding(
+                        padding: EdgeInsets.symmetric(vertical: 16),
+                        child: Text(
+                          'ou',
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      // GoogleButton
+                      InkWell(
+                        onTap: () {
+                          //Vai pro login com google
+                        },
+                        child: Container(
+                          child: Image.asset(
+                            AppImages.googleIcon,
+                            width: 60,
+                            height: 50,
+                            fit: BoxFit.contain,
+                          ),
+                        ),
+                      ),
+
+                      ///CadastroButton
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Text(
+                            'Não possui uma conta?',
+                            style: TextStyle(fontSize: 15),
+                          ),
+                          TextButton(
+                              onPressed: () {},
+                              child: Text(
+                                'Cadastre-se aqui!',
+                                style: TextStyle(
+                                    fontSize: 15, color: AppColors.orange),
+                              )),
+                        ],
                       )
-                      ),
                     ],
-                  )
-
-                ],
+                  ),
+                ),
               ),
-            ),
-          )
-        ],
-      ) 
-
-    );
+            )
+          ],
+        ));
   }
 }
