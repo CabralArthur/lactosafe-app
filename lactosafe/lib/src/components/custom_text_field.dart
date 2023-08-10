cc770e0ae20270ef4340433ce9079a69a08ba79b
João Vitor da Silva Pires <94564034+JVitorAccelerator@users.noreply.github.com>
2023-08-10 09:17:52 -0300

    Update custom_text_field.dart
    Update to receive the controller

--- lactosafe/lib/src/components/custom_text_field.dart
+++ lactosafe/lib/src/components/custom_text_field.dart
@@ -1,31 +1,36 @@
-import 'package:flutter/material.dart';
-
 class CustomTextField extends StatelessWidget {
   final IconData icon;
   final String label;
   final bool isObscure;
   final String? Function(String?)? validator;
+  final TextEditingController controller; // Add this line
 
-
-
-  const CustomTextField({super.key, required this.icon, required this.label,  this.isObscure = false, this.validator});
+  const CustomTextField({
+    Key? key,
+    required this.icon,
+    required this.label,
+    this.isObscure = false,
+    this.validator,
+    required this.controller, // Add this line
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(bottom: 15),
       child: TextFormField(
-                      validator: validator,
-                      obscureText: isObscure,
-                      decoration: InputDecoration(
-                        prefixIcon:  Icon(icon),
-                        labelText: label,
-                        isDense: true,
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(18)
-                        )
-                      ),
-                    ),
+        controller: controller, // Add this line
+        validator: validator,
+        obscureText: isObscure,
+        decoration: InputDecoration(
+          prefixIcon: Icon(icon),
+          labelText: label,
+          isDense: true,
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(18),
+          ),
+        ),
+      ),
     );
   }
 }
