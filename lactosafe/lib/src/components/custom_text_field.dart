8682847df36567fd6efb68f59e8a408cd4bc7ec1
João Vitor da Silva Pires <jvitorpires1999@gmail.com>
2023-08-11 19:19:20 -0300

    Correcting the custom_text_field

--- lactosafe/lib/src/components/custom_text_field.dart
+++ lactosafe/lib/src/components/custom_text_field.dart
@@ -1,9 +1,11 @@
+import 'package:flutter/material.dart';
+
 class CustomTextField extends StatelessWidget {
   final IconData icon;
   final String label;
   final bool isObscure;
   final String? Function(String?)? validator;
-  final TextEditingController controller; // Add this line
+  final TextEditingController controller;
 
   const CustomTextField({
     Key? key,
@@ -11,7 +13,7 @@ class CustomTextField extends StatelessWidget {
     required this.label,
     this.isObscure = false,
     this.validator,
-    required this.controller, // Add this line
+    required this.controller,
   }) : super(key: key);
 
   @override
@@ -19,7 +21,7 @@ class CustomTextField extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.only(bottom: 15),
       child: TextFormField(
-        controller: controller, // Add this line
+        controller: controller,
         validator: validator,
         obscureText: isObscure,
         decoration: InputDecoration(
