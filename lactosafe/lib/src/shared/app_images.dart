1e997eea2a13c921c33d4c4b0cb29263e1dce2a3
GrazielaFelix <grazielafelix.dev@gmail.com>
2023-08-11 14:16:34 -0300

    feat: emojis e menu inferior icons

--- lactosafe/lib/src/shared/app_images.dart
+++ lactosafe/lib/src/shared/app_images.dart
@@ -6,5 +6,14 @@ class AppImages {
   static String userPicture = "assets/images/userPhoto.png";
   static String userDefault = "assets/images/UserProfile.png";
   static String logoLetterless = "assets/images/lactosafe_logo_letterless.png";
+  static String smileEmoji = "assets/images/SmileEmoji.svg";
+  static String doubtEmoji = "assets/images/DoubtEmoji.svg";
+  static String medianEmoji = "assets/images/MedianEmoji.svg";
+  static String surprisedEmoji = "assets/images/SurprisedEmoji.svg";
+  static String sadEmoji = "assets/images/SadEmoji.svg";
+  static String welcome = "assets/images/Welcome.png";
+  static String cameraIcon = "assets/images/cameraIcon.png";
+  static String homeIcon = "assets/images/homeIcon.png";
+  static String mapIcon = "assets/images/mapIcon.png";
 
 }
