b4079dc37ab482efd7002f97627a57e00db2742e
GrazielaFelix <grazielafelix.dev@gmail.com>
2023-08-13 20:42:10 -0300

    feat: nova cor background

--- lactosafe/lib/src/shared/app_colors.dart
+++ lactosafe/lib/src/shared/app_colors.dart
@@ -5,4 +5,5 @@ class AppColors {
   static Color grey = const Color(0x991E1E1E);
   static Color shadow = const Color.fromARGB(15, 0, 0, 0);
   static Color green = const Color(0x9928DF00);
+  static Color backgroundColor = const  Color(0xFFF8F8F8);
 }
