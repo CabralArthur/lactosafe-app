01b25f208c2870b2f3802e39fc7dd6575b54b03f
GrazielaFelix <grazielafelix.dev@gmail.com>
2023-09-04 17:39:19 -0300

    refactor: logica para realizar o login do usuario no app

--- lactosafe/lib/src/controller/singin_page_controller.dart
+++ lactosafe/lib/src/controller/singin_page_controller.dart
@@ -1,8 +1,11 @@
+import 'package:LactoSafe/src/http/exceptions.dart';
+import 'package:LactoSafe/src/model/user_model.dart';
+import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
-Future<bool> login(String email, String password) async {
-  var url = Uri.parse('http://192.168.5.129:5000/login');
+Future login(String email, String password, BuildContext context) async {
+  var url = Uri.parse('http://192.168.0.109:5000/login');
 
   var loginData = {'email': email, 'senha': password};
   var headers = {'Content-Type': 'application/json'};
@@ -11,8 +14,19 @@ Future<bool> login(String email, String password) async {
       await http.post(url, headers: headers, body: json.encode(loginData));
 
   if (response.statusCode == 200) {
-    return true;
+    final decodedUser = jsonDecode(response.body);
+    if (decodedUser['isLogged'] == true) {
+      User user = User.fromMap(decodedUser);
+      userStore = user;
+    
+      if (context.mounted) {
+        Navigator.popAndPushNamed(context, '/home');
+      }
+    } else if (decodedUser['isLogged'] == false) {
+      
+      return decodedUser;
+    }
   } else {
-    return false;
+    return NotFoundExeption('Algum erro ocorreu');
   }
 }
