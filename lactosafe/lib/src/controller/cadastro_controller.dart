9a10d71106fb03f5a6237bef7427acf583a0a84c
GrazielaFelix <grazielafelix.dev@gmail.com>
2023-09-05 19:11:36 -0300

    refactor: ajustes no response da api

--- lactosafe/lib/src/controller/cadastro_controller.dart
+++ lactosafe/lib/src/controller/cadastro_controller.dart
@@ -4,30 +4,33 @@ import 'dart:convert';
 
 Future Cadastro(String email, String password, String username, String endereco,
     String intolerancia, BuildContext context) async {
-  var url = Uri.parse('http://127.0.0.1:5000/cadastro');
+  var url = Uri.parse('http://192.168.0.109:5000/cadastro');
 
   var cadastroData = {
     'email': email,
     'senha': password,
-    'username': username,
-    'endereco': endereco,
-    'intolerancia': intolerancia,
+    //Atualmente a API só recebe os campo email e senha - 05/09/23
+    // 'username': username,
+    // 'endereco': endereco,
+    // 'intolerancia': intolerancia,
   };
 
   var headers = {'Content-Type': 'application/json'};
 
-  try {
-    var response =
-        await http.post(url, headers: headers, body: json.encode(cadastroData));
 
+  try {
+      var response =
+        await http.post(url, headers: headers, body: json.encode(cadastroData));
     if (response.statusCode == 200) {
       final decodedUser = jsonDecode(response.body);
 
       if (decodedUser['isRegistered'] == true) {
         // ignore: use_build_context_synchronously
-        Navigator.popAndPushNamed(context, '/welcomePage');
+        return decodedUser['isRegistered'];
+        
       } else {
-        return 'O cadastro falhou: ${decodedUser['error_message']}';
+        //Configurar melhor quando o usuario já estiver cadastrado
+        return 'O cadastro falhou: ${decodedUser['message']}';
       }
     } else {
       throw Exception('Falha na requisição: ${response.statusCode}');
